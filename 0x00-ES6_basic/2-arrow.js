16e99c30c0f0ab8f770e7628088b52b9c8f46b97
heyeman <abdisaheyeman083@gmail.com>
2021-11-22 21:31:25 +0300

    update

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  self.addNeighborhood =  add = (newNeighborhood) => {
+  self.addNeighborhood =  (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
