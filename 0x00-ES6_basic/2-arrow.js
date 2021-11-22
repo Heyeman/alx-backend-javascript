33fc6f04391523b53aaefb848e3d3aeedd374838
heyeman <abdisaheyeman083@gmail.com>
2021-11-22 18:13:05 +0300

    self instead of this

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  self.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood =  add = (newNeighborhood) => {
+  self.addNeighborhood =  add = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
