f6179a0d055c682dc55237dc8f676e9462437c83
heyeman <abdisaheyeman083@gmail.com>
2021-12-02 16:52:27 +0300

    made the annotation and definition of the variables on different lines:

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,11 @@
 """Contains 4 variables with the proper annotations"""
 
 
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = "Holberton"
+a: int
+a = 1
+pi: float
+pi = 3.14
+i_understand_annotations: bool
+i_understand_annotations = True
+school: str
+school = "Holberton"
