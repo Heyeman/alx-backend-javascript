2c4963ca293cf21836e1457f91b35c89f56e2b1b
heyeman <abdisaheyeman083@gmail.com>
2022-04-17 18:45:54 +0000

    returned

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise()
+  return (promise()
     .then(() => ({ status: 200, body: 'success' }))
     .catch(
       () => new Error(''),
@@ -7,5 +7,5 @@ export default function handleResponseFromAPI(promise) {
     )
     .finally(() => {
       console.log('Got a response from the API');
-    });
+    }));
 }
