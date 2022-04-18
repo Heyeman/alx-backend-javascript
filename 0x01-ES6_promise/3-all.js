09e901ebfd6c7315ff18ba53907817abd7c383c0
heyeman <abdisaheyeman083@gmail.com>
2022-04-18 22:37:49 +0000

    optimised

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,11 @@
-import * as func from './utils';
+import {uploadPhoto, createUser} from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([func.uploadPhoto(), func.createUser()])
+    let create = createUser()
+    let upload = uploadPhoto()
+    return Promise.all([upload, create])
     .then((data) => {
-      console.log(data[0].body, data[1].firstName, data[1].lastName);
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
