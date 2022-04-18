7cc55585828bef7cc9f135ff071c871dca4103a5
heyeman <abdisaheyeman083@gmail.com>
2022-04-18 22:38:47 +0000

    linted

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
-import {uploadPhoto, createUser} from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    let create = createUser()
-    let upload = uploadPhoto()
-    return Promise.all([upload, create])
+  const create = createUser();
+  const upload = uploadPhoto();
+  return Promise.all([upload, create])
     .then((data) => {
       console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
