594e3cc5af94b9ddb9b675a76662f59a7ce1bc7b
heyeman <abdisaheyeman083@gmail.com>
2022-04-19 09:04:34 +0000

    E

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,51 +1,48 @@
-export default class HolbertonCourse{
-    /* eslint no-underscore-dangle: 0 */
-    constructor(name, length, students) {
-	this.name(name);
-	this.length(length);
-	 this.students(students);
-	
-	
+export default class HolbertonCourse {
+  /* eslint no-underscore-dangle: 0 */
+  constructor(name, length, students) {
+    const _name = '';
+    const _length = '';
+    const _students = '';
+
+    this.name(name);
+    this.length(length);
+    this.students(students);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof (newName) === 'string') {
+      const _name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
-    get name() {
-	return this._name; }
-    set name(newName)
-    {
-	    if (typeof(newName) == "string"){
-	    let _name = newName;
-	}
-	else{
-	    throw new TypeError("Name must be a string");
+  }
 
-	}
+  get length() {
+    return this.length;
+  }
+
+  set length(newLength) {
+    if (typeof (newLength) === 'number') {
+      const _length = newLength;
+    } else {
+      throw new TypeError('Length must be a number');
     }
-	get length(){
-		return this.length;}
-	set length(newLength) {
-	if (typeof(newLength) == "number"){
-	    let _length = newLength;
-	}
-	else{
-	    throw new TypeError("Length must be a number");
+  }
 
-	}
-	}
-	get students(){
-	return this._students;}
-	set students(newStudents){
-	if (typeof(newStudents) == "object"){
-	    let _students = newStudents;
-	}
-	else{
-	    throw new TypeError("Students must be an array");
+  get students() {
+    return this._students;
+  }
 
-	}
-	}  
-
+  set students(newStudents) {
+    if (typeof (newStudents) === 'object') {
+      const _students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
 }
-	
-	
-	
-	
-	
-    
