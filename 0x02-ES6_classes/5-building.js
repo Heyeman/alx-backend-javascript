de60c8f5a9fef1680365b339ee4808ffc381eff4
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-04-30 23:57:19 +0300

    file 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 /* eslint no-underscore-dangle: 0 */
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && typeof ('evacuationWarningMessage') === 'undefined') {
+    this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
