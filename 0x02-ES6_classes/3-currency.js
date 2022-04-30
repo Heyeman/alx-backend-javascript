81003b4c87b559e896bf902c8d4aae386bcda7c4
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-04-30 22:53:00 +0300

    added file 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,21 @@
+export default class Currency{
+    constructor(code, name){
+        this.code = _code;
+        this.name = _name;
+    }
+    get code(){
+        return this._code;
+    }
+    get name(){
+        return this._name;
+    }
+    set code(value){
+        this._name = value;
+    }
+    set name(value){
+        this._name = value;
+    }
+    displayFullCurrency(){
+    return `${this.name} (${this.code})`;
+    }
+}
