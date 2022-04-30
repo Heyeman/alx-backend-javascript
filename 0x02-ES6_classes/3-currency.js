8d300022ba44e5c870993f646064a16f3df6e8cc
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-04-30 23:13:41 +0300

    linted

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,22 +1,27 @@
-export default class Currency{
-    constructor(code, name){
-        this._code = code;
-        this._name = name;
-    }
-    get code(){
-        return this._code;
-    }
-    get name(){
-        return this._name;
-    }
-    set code(value){
-        this.code = value;
-    }
-    set name(value){
-        this._name = value;
-    }
-    
-    displayFullCurrency(){
+/* eslint no-underscore-dangle: 0 */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(value) {
+    this.code = value;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
     return `${this._name} (${this._code})`;
-    }
+  }
 }
