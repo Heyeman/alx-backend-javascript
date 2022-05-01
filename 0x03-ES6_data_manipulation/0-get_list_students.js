6a435d89db63d0eb8eca222e0ee377fbfa0b8cf2
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-01 11:30:37 +0300

    f1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,3 @@
-const getListStudents = () => {
-  const students = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
-  return students;
-};
+export default function getListStudents() {
+  return [{ id: 1, firstName: 'Guillaume', Location: 'San Francisco' }, { id: 2, firstName: 'James', Location: 'Columbia' }, { id: 5, firstName: 'Serena', Location: 'San Francisco' }];
+}
