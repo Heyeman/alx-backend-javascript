78442bba3f8252e3c1dbafed09027edb24a4a78e
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-01 15:55:29 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 const cleanSet = (set, startString) => {
+  if (startString.length === 0) {
+    return '';
+  }
   const arr = [];
   set.forEach((element) => {
     if (startString.length && element.slice(0, startString.length) === startString) {
